74a6d496b733f4dd10243b495b782116be389621
baadalvm <sumeetksinha>
2025-12-29 17:26:14 +0530

    Updated from                        the server

--- app.py
+++ app.py
@@ -518,4 +518,4 @@ def serve():
 if __name__ == '__main__':
     # app.jinja_env.auto_reload = True
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, '/home/baadalvm/CSMIP')
+
+activate_this = '/home/baadalvm/CSMIP'
+
+
+from app import app as application
+
